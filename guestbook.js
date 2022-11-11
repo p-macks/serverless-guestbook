f503fa873ac137ad04f905b3b45431847578c703
p-macks <112434143+p-macks@users.noreply.github.com>
2022-11-11 19:17:10 +0200

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/patiemacks_Dallas/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
